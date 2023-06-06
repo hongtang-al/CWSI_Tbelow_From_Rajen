281176c575f5922062604a63eb434cd3921cb159
hongtang_al <hong.tang@arable.com>
2023-06-06 09:21:55 -0700

    fix plot level concat remove duplicates

--- CWSI_Field_Trial_Model_Prediction.py
+++ CWSI_Field_Trial_Model_Prediction.py
@@ -1,5 +1,10 @@
 
 # %%
+# !pip install plotly
+# !pip install nbformat --upgrade
+import nbformat
+print(nbformat.__version__)
+
 import sys
 import pandas as pd
 # from matplotlib import pyplot as plt
@@ -63,8 +68,8 @@ def create_features(field_df):
     # field_df['tzconvert_time1'] = field_df['time'].dt.tz_convert('America/Los_Angeles')
     # field_df['fntrans_time'] = solar_noon_(field_df['time'], field_df['long'])
     field_df['solar_time'] = get_solar_time( field_df['long'], field_df['time'])
-    field_df['solarnoon'] = (field_df['solar_time'].dt.hour >= 11) & (field_df['solar_time'].dt.hour < 14)
-
+    field_df['solarnoon'] = (field_df['solar_time'].dt.hour >= 12) & (field_df['solar_time'].dt.hour < 16)
+    field_df['solarnoon'] = field_df['solarnoon'].astype(int)
     return field_df
 # %%
 #dataframe is pulled using SQL query directly using: CWSI_Ref_Mark_Joined_data_ETL.py
@@ -73,28 +78,37 @@ path = 'Carbon Project/Stress Index/UCD_Almond/Joined_ref_df_hourly.csv'
 
 # Load DataFrame from S3 bucket
 joined_df = df_from_s3(path, bucket_name, format='csv')
+
 # %%
 # change columns names to use following processing program
 field_df=joined_df.drop(['ref_time', 'body_temp'], axis=1)
+
 # Create the 'tbelow-tair' and es
 create_features(field_df)
 # Drop rows with missing values
 field_df.dropna(inplace=True) 
+
 # %%
+regline_list=[]
 res_df = pd.DataFrame()
 sites = field_df.site_id.unique().tolist()
-# %%
 #create CWSI
 for site in sites:
     #select time window to compute m1_coef_, m1_intercept_
-    solarnoon_mask=(field_df.solarnoon) & (field_df.time> '2023-05-06') & (field_df.time> '2023-05-06')
-    x, Y_pred, m1_coef_, m1_intercept_ = lr_vpd_tdif(field_df[(field_df['site_id'] == site)  & solarnoon_mask][['vpd', 'ref_tbelow-tair']])
-    _ = preprocessing(field_df[field_df['site_id'] == site], m1_coef_[0][0], m1_intercept_[0])
-    res_df = pd.concat([res_df, _])
-    print(site, m1_coef_, m1_intercept_)
+    for source in ['L1','L2', 'H1', 'H2']:
+        solarnoon_mask=(field_df.solarnoon==1) & (field_df.time> '2023-05-06') & (field_df.time> '2023-05-06')
+        # compute slope and interception for solarnoon on each device
+        x, Y_pred, m1_coef_, m1_intercept_ = lr_vpd_tdif(field_df[(field_df['site_id'] == site) \
+                                                                & (field_df['source'] == source) \
+                                                                & solarnoon_mask][['vpd', 'ref_tbelow-tair']])
+        plotmask=(field_df['site_id'] == site) & (field_df['source'] == source)
+        _ = preprocessing(field_df[plotmask], m1_coef_[0][0], m1_intercept_[0])
+        res_df = pd.concat([res_df, _])
+        print(site, source, m1_coef_[0][0], m1_intercept_[0], res_df.shape)
+        regline_list.append([site, source, m1_coef_[0][0], m1_intercept_[0]])
 
 res_df = res_df.drop(['diff'], axis=1)
-res_df 
+regline_list
 
 # %%
 bucket_name = 'arable-adse-dev'
@@ -103,8 +117,6 @@ path = 'Carbon Project/Stress Index/UCD_Almond/field_cwsi_trial.csv'
 df_to_s3(res_df, path, bucket_name, format='csv')
 
 # %%
-res_df
-# %%
 res_df.to_csv('./data/cwsi_joined.csv')
 
 # %%
@@ -112,7 +124,7 @@ res_df.to_csv('./data/cwsi_joined.csv')
 import plotly.express as px
 def soloarnoon_vpd_delta(res_df, interception,slope ):
     # Assuming you have a DataFrame called res_df with columns 'vpd' and 'ref_tbelow-tair'
-    fig = px.scatter(res_df.loc[res_df.solarnoon], x='vpd', y='ref_tbelow-tair', color='source')
+    fig = px.scatter(res_df.loc[res_df.solarnoon==1], x='vpd', y='ref_tbelow-tair', color='source')
     fig.add_trace(px.line(x=[res_df['vpd'].min(), res_df['vpd'].max()],
                         y=[interception + slope * res_df['vpd'].min(),
                             interception + slope * res_df['vpd'].max()]).data[0])
@@ -120,12 +132,44 @@ def soloarnoon_vpd_delta(res_df, interception,slope ):
     fig.update_layout(title=f"Scatter Plot - Site IDs: {site_id_title}")
     fig.show()
 
-interception = 5.12769509
-slope = 0.28445425
-df = res_df.loc[res_df['site_id']=='TWE_GB']
-soloarnoon_vpd_delta(df, interception,slope )
+for item in regline_list:
+    print(item)
+    df = res_df.loc[(res_df['site_id']==item[0]) & (res_df['source']==item[1])]
+    soloarnoon_vpd_delta(df, item[3],item[2] )
 
-interception = 4.231586
-slope = 0.13272558
-df = res_df.loc[res_df['site_id']=='TWE_BV2']
-soloarnoon_vpd_delta(df, interception,slope )
+
+# %%
+
+# plot all regression lines in one plot
+import plotly.graph_objects as go
+
+# Create an empty figure
+fig = go.Figure()
+
+for item in regline_list:
+    df = res_df.loc[(res_df['site_id']==item[0]) & (res_df['source']==item[1])]
+
+    # Add a scatter trace for each group of data
+    # fig.add_trace(
+    #     go.Scatter(
+    #         x=df['vpd'],
+    #         y=df['ref_tbelow-tair'],
+    #         mode='markers',
+    #         name=f"site_id: {item[0]}, source: {item[1]}"
+    #     )
+    # )
+    # Add a line trace for the regression line
+    fig.add_trace(
+        go.Scatter(
+            x=[df['vpd'].min(), df['vpd'].max()],
+            y=[item[3] + item[2] * df['vpd'].min(),
+               item[3] + item[2] * df['vpd'].max()],
+            mode='lines',
+            name=f"Regression Line: site_id {item[0]}, source: {item[1]}"
+        )
+    )
+
+fig.update_layout(title="Scatter Plot for all Site IDs")
+fig.show()
+
+# %%
